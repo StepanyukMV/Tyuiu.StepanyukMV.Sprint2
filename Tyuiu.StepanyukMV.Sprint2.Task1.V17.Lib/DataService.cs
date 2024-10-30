431e480a7bdba49dcc90030a2939d0111ef8448f
StepanyukMV <stepanyukmv@outlook.com>
2024-10-30 11:21:53 +0500

    Task1

---
+++ Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = b + 198 == d | a == b;
+            res[1] = (a > c) & (b < d);
+            res[2] = a + 320 > c || d > b;
+            res[3] = (a > c) && (b < d);
+            res[4] = !(!res[0]);
+            res[5] = (a > c) ^ (b < d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.StepanyukMV.Sprint2.Task1.V17/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib;
+namespace Tyuiu.StepanyukMV.Sprint2.Task1.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Степанюк М.В. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Степанюк Михаил Вадимович | РППб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз           *");
+            Console.WriteLine("* в выражении) и логических операций (|, &, ||, &&, !, ^,                 *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, False, True, True, True, False), при a = 135, b = 123, c = 455.  *");
+            Console.WriteLine("* d = 321.                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" A = 135 ");
+            Console.WriteLine(" B = 123 ");
+            Console.WriteLine(" C = 455 ");
+            Console.WriteLine(" D = 321 ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+        }
+    }
+}
