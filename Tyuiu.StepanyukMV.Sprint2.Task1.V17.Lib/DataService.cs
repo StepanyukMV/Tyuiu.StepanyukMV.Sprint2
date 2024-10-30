aaed4342f6a1c2a36744138afdb77d77220c5b66
StepanyukMV <stepanyukmv@outlook.com>
2024-10-30 11:24:00 +0500

    Task1

--- Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib/DataService.cs
+++ Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.StepanyukMV.Sprint2.Task1.V17.Lib
             res[0] = b + 198 == d | a == b;
             res[1] = (a > c) & (b < d);
             res[2] = a + 320 > c || d > b;
-            res[3] = (a > c) && (b < d);
+            res[3] = (a < c) && (b < d);
             res[4] = !(!res[0]);
             res[5] = (a > c) ^ (b < d);
             return res;
